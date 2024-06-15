0b92438fb7786a8cae942143a1fb38000bf6787e
srabhiram <sriramoji.abhi@gmail.com>
2024-06-15 20:52:12 +0530

    bug fixes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Skills = () => {
   return (
     <section
-      className="mt-16 px-3"
+      className="mt-20 px-3"
       id="skills"
       data-aos="fade-up"
       data-aos-delay="200"
@@ -17,27 +17,27 @@ const Skills = () => {
         <div className="flex gap-[50px] flex-wrap mt-4 px-4 py-3">
           <i
             title="HTML5"
-            class="fa-brands fa-html5 text-[70px] text-green-500"
+            class="fa-brands fa-html5 text-[70px] text-green-600"
           ></i>
           <i
             title="CSS3"
-            class="fa-brands fa-css3-alt text-[70px] text-green-500"
+            class="fa-brands fa-css3-alt text-[70px] text-green-600"
           ></i>
           <i
             title="JavaScript"
-            class="fa-brands fa-js text-[70px] text-green-500"
+            class="fa-brands fa-js text-[70px] text-green-600"
           ></i>
           <i
             title="ReactJS"
-            class="fa-brands fa-react text-[70px] text-green-500"
+            class="fa-brands fa-react text-[70px] text-green-600"
           ></i>
           <i
             title="NodeJS"
-            class="fa-brands fa-node-js text-[70px] text-green-500"
+            class="fa-brands fa-node-js text-[70px] text-green-600"
           ></i>
           <i
             title="Git"
-            class="fa-brands fa-git text-[70px] text-green-500"
+            class="fa-brands fa-git text-[70px] text-green-600"
           ></i>
         </div>
       </div>
